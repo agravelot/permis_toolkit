f6a16125a2a7ffa748c475369675c77c19e285cc
Antoine GRAVELOT <a.gravelot@hotmail.fr>
2021-10-01 21:09:36 +0200

    cleanup dependencies

--- cmd/login/main.go
+++ cmd/login/main.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"github.com/agravelot/permis_toolkit/ornikar"
-	"github.com/davecgh/go-spew/spew"
 )
 
 func main() {
 	// TODO pass env
 	token, _ := ornikar.Login("", "")
 
-	spew.Dump(token)
+	println(token)
 }
